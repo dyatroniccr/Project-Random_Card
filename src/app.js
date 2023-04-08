d7e77f93f616a597e825f0865ac50fd704c076d9
Jonathan A. Montero C <dyatroniccr@gmail.com>
2023-04-08 18:47:55 +0000

    Update

--- src/app.js
+++ src/app.js
@@ -21,17 +21,17 @@ window.onload = function() {
   //write your code here
 
   const quest1 = document.querySelector("#quest1");
-  quest1.innerHTML = randomNum(who);
+  quest1.innerHTML = "<h5 >" + randomNum(who) + "</h5>";
   const quest2 = document.querySelector("#quest2");
-  quest2.innerHTML = randomNum(action);
+  quest2.innerHTML = "<h5>" + randomNum(action) + "</h5>";
   const quest3 = document.querySelector("#quest3");
-  quest3.innerHTML = randomNum(what);
+  quest3.innerHTML = "<h5>" + randomNum(what) + "</h5>";
   const quest4 = document.querySelector("#quest4");
-  quest4.innerHTML = randomNum(when);
+  quest4.innerHTML = "<h5>" + randomNum(when) + "</h5>";
 
-  when.map((item, index) => {
+  /*when.map((item, index) => {
     quest4.innerHTML = `<div class="col border" key="${index}"> ${item} </div>`;
-  });
+  });*/
 };
 
 function randomNum(arrSelect) {
